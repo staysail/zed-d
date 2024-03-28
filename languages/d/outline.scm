77cabf4b81d6feca41a2f054dbaaf5260c563635
Garrett D'Amore <garrett@damore.org>
2024-03-27 21:15:35 -0700

    fixes #2 Outline does not detect auto function

--- languages/d/outline.scm
+++ languages/d/outline.scm
@@ -1,4 +1,4 @@
-(function_declaration (type) @context (identifier) @name (template_parameters)? @context (parameters) @context (function_body)) @item
+(function_declaration (type)? @context (identifier) @name (template_parameters)? @context (parameters) @context (function_body)) @item
 (class_declaration (identifier) @name (template_parameters)? @context (aggregate_body)) @item
 (interface_declaration (identifier) @name (template_parameters)? @context (aggregate_body)) @item
 (struct_declaration (identifier) @name (template_parameters)? @context (aggregate_body)) @item
