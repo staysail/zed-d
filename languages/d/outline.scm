e5b53768dae847b7eed057421373075f151ac9d9
Garrett D'Amore <garrett@damore.org>
2024-03-27 21:40:51 -0700

    fixes #3 Constructors not identified in outlines
    This also fixes destructors and postblits.

--- languages/d/outline.scm
+++ languages/d/outline.scm
@@ -2,4 +2,8 @@
 (class_declaration (identifier) @name (template_parameters)? @context (aggregate_body)) @item
 (interface_declaration (identifier) @name (template_parameters)? @context (aggregate_body)) @item
 (struct_declaration (identifier) @name (template_parameters)? @context (aggregate_body)) @item
-(unittest_declaration (unittest) @name (block_statement)) @item
+(constructor (this) @name (template_parameters)? @context (parameters) @context (function_body)) @item
+(constructor (shared)? @context (static) @context (this) @name "(" @context ")" @context (function_body)) @item
+(postblit (this) @name "(" @context (this) @context ")" @context (function_body)) @item
+(destructor "~" @context (this) @name (function_body)) @item
+(unittest_declaration (unittest) @context (block_statement)) @item
